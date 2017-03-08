e6026456a1aab51f9eb202040946eb01dc85108c
Tim Beurskens <timbeurskens.97@gmail.com>
2017-03-08 14:37:03 +0100

    create threading example

--- KinectDemo/KinectDemo/Program.cs
+++ KinectDemo/KinectDemo/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace KinectDemo
@@ -10,6 +11,33 @@ namespace KinectDemo
     {
         static void Main(string[] args)
         {
+            Thread t = new Thread(new ThreadStart(DiffucultComputation)); // specify which method to run in a different thread
+            t.Start(); // start the thread
+
+            while (t.IsAlive) // while this thread is running, the main thread is able to do some other things
+            {
+                Thread.Sleep(1000); // pause all computations on the main thread
+                Console.WriteLine("Still doing some difficult stuff"); // print something in the main thread
+            }
+
+            Console.WriteLine("Computation done");
+        }
+
+        /// <summary>
+        /// This method will take some time to run.
+        /// </summary>
+        static void DiffucultComputation()
+        {
+            Console.WriteLine("Starting some difficult computation");
+            long result = 0;
+            for (int x = 0; x < 100000; x++)
+            {
+                for(int y = 0; y < 20000; y++)
+                {
+                    result++;
+                }
+            }
+            Console.WriteLine("Computation done, result: " + result);
         }
     }
 }
