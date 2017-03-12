db6b164c57f00d9e731002fecd5cab79ab258622
Tim Beurskens <timbeurskens.97@gmail.com>
2017-03-12 21:18:09 +0100

    implement functionality

--- KinectDemo/KinectDemo/Program.cs
+++ KinectDemo/KinectDemo/Program.cs
@@ -1,43 +1,85 @@
 ﻿using Microsoft.Kinect;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KinectDemo
 {
-    class Program
+    /// <summary>
+    /// Static example on how to read body data
+    /// The programming paradigms used in this example are not recommended
+    /// </summary>
+    internal class Program
     {
-        static KinectSensor sensor = null;
+        private delegate void BodyReadyEventHandler(Body b);
+        private static KinectSensor _sensor;
+        private static IList<Body> _bodies;
+        private static event BodyReadyEventHandler BodyReady;
 
-        static void Main(string[] args)
+        private static void Main()
         {
-            sensor = KinectSensor.GetDefault(); // get singleton instance of the KinectSensor
-            BodyFrameReader bodyFrameReader = sensor.BodyFrameSource.OpenReader(); // get body frame reader
+            _sensor = KinectSensor.GetDefault(); // get singleton instance of the KinectSensor
+            var bodyFrameReader = _sensor.BodyFrameSource.OpenReader(); // get body frame reader
 
             // event handlers
             bodyFrameReader.FrameArrived += Reader_FrameArrived;
-            sensor.IsAvailableChanged += Sensor_IsAvailableChanged;
+            _sensor.IsAvailableChanged += Sensor_IsAvailableChanged;
             AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
+            BodyReady += Program_BodyReady;
 
             // open sensor
-            sensor.Open();           
+            _sensor.Open();
+
+            while (_sensor.IsOpen)
+            {
+                // close button: x key
+                if (Console.ReadKey().Key == ConsoleKey.X) _sensor.Close();
+            }
         }
 
-        static void Reader_FrameArrived(object sender, BodyFrameArrivedEventArgs args)
+        /// <summary>
+        /// Handles body data
+        /// </summary>
+        /// <param name="body">body data from kinect sensor</param>
+        private static void Program_BodyReady(Body body)
         {
-            Console.WriteLine(args.FrameReference.AcquireFrame().BodyCount);
+            foreach (var joint in body.Joints)
+            {
+                if (joint.Value.TrackingState == TrackingState.NotTracked || joint.Value.TrackingState == TrackingState.Inferred) continue; //joint is not tracked, so skip it for now
+
+                // print 3D joint information
+                Console.Write(joint.Key);
+                Console.Write(" - ");
+                Console.WriteLine("(" + joint.Value.Position.X + ", " + joint.Value.Position.Y + ", " + joint.Value.Position.Z + ")");
+            }
         }
 
-        static void Sensor_IsAvailableChanged(object sender, IsAvailableChangedEventArgs args)
+        private static void Reader_FrameArrived(object sender, BodyFrameArrivedEventArgs args)
+        {
+            using (var bodyFrame = args.FrameReference.AcquireFrame())  //disposes variable after this block statement
+            {
+                _bodies = new Body[bodyFrame.BodyCount];
+
+                bodyFrame.GetAndRefreshBodyData(_bodies); // fill _bodies list with body data
+
+                foreach (var body in _bodies) // for every body
+                {
+                    if (!body.IsTracked) continue; // body is not tracked, skip body
+
+                    BodyReady?.BeginInvoke(body, null, null);
+
+                    break; //when one body is handled, break out of this loop
+                }
+            }
+        }
+
+        private static void Sensor_IsAvailableChanged(object sender, IsAvailableChangedEventArgs args)
         {
             Console.WriteLine(args.IsAvailable);
         }
 
-        static void OnProcessExit(object sender, EventArgs args)
+        private static void OnProcessExit(object sender, EventArgs args)
         {
-            sensor.Close();
+            _sensor.Close();
         }
     }
 }
