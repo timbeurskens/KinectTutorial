d3e8a93376ee48b364308e9503ca7eebc2f655b3
Tim Beurskens <timbeurskens.97@gmail.com>
2017-03-13 10:08:48 +0100

    implement event example

--- KinectDemo/KinectDemo/Program.cs
+++ KinectDemo/KinectDemo/Program.cs
@@ -1,15 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace KinectDemo
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public delegate void MyEventDelegate(object sender, EventArgs e); // define how an event listener should look like
+
+        public event MyEventDelegate MyEvent; // the event
+
+        private static void Main()
         {
+            var instance = new Program(); // create instance of program
+
+            instance.MyEvent += Instance_MyEvent; // add an event-listener
+
+            instance.Run(); // start program
+        }
+
+        /// <summary>
+        /// Main method for the program instance
+        /// </summary>
+        public void Run()
+        {
+            //do some stuff
+            Thread.Sleep(100);
+
+            OnMyEvent(); //fire event
+        }
+
+        /// <summary>
+        /// The event listener
+        /// </summary>
+        /// <param name="sender">Sender object of MyEvent</param>
+        /// <param name="e">Event arguments, in this case empty</param>
+        private static void Instance_MyEvent(object sender, EventArgs e)
+        {
+            Console.WriteLine("Event!");
+        }
+
+        /// <summary>
+        /// Fires the MyEvent event
+        /// </summary>
+        protected virtual void OnMyEvent()
+        {
+            MyEvent?.Invoke(this, EventArgs.Empty);
         }
     }
 }
